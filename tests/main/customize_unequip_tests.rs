c314d22580c6487d025ad08aacbc894bc3cc010e
TF <31354550+Theo-Farnole@users.noreply.github.com>
2022-06-20 13:49:33 +0200

    ADD ignore case test in unequip function

--- tests/main/customize_unequip_tests.rs
+++ tests/main/customize_unequip_tests.rs
@@ -110,6 +110,103 @@ fn customize_only_unequip() {
 }
 
 #[test]
+fn unequip_should_ignore_case_of_slot() {
+    // 1. ARRANGE
+    let mut setup = testing_utils::setup(customize_nft::contract_obj);
+
+    const SLOT_LOWERCASE: &[u8] = b"background";
+    const SLOT_UPPERCASE: &[u8] = b"BACKGROUND";
+    const ITEM_TO_UNEQUIP_ID: &[u8] = b"BG-a1a1a1";
+    const ITEM_TO_UNEQUIP_NAME: &[u8] = b"Some Item";
+    const NONCE: u64 = 30;
+
+    DebugApi::dummy();
+
+    setup.create_equippable_with_registered_item(
+        NONCE,
+        ITEM_TO_UNEQUIP_ID,
+        NONCE,
+        SLOT_LOWERCASE,
+        ItemAttributes::random(),
+    );
+
+    setup
+        .blockchain_wrapper
+        .execute_tx(
+            &setup.owner_address,
+            &setup.cf_wrapper,
+            &rust_biguint!(0),
+            |sc| {
+                sc.ipfs_gateway()
+                    .set(ManagedBuffer::new_from_bytes(b"https://ipfs.io/ipfs/"));
+
+                let attributes_before_custom = EquippableNftAttributes::new(&[(
+                    &ManagedBuffer::new_from_bytes(SLOT_LOWERCASE),
+                    Item {
+                        token: TokenIdentifier::<DebugApi>::from_esdt_bytes(ITEM_TO_UNEQUIP_ID),
+                        nonce: NONCE,
+                        name: ManagedBuffer::new_from_bytes(ITEM_TO_UNEQUIP_NAME),
+                    },
+                )]);
+
+                sc.set_cid_of(
+                    &attributes_before_custom,
+                    ManagedBuffer::<DebugApi>::new_from_bytes(b"this is a cid"),
+                );
+
+                sc.set_cid_of(
+                    &EquippableNftAttributes::<DebugApi>::empty(),
+                    ManagedBuffer::new_from_bytes(b"empty"),
+                );
+            },
+        )
+        .assert_ok();
+
+    let transfers = testing_utils::create_esdt_transfers(&[(EQUIPPABLE_TOKEN_ID, NONCE)]);
+
+    // 2. ACT
+    let (sc_result, tx_result) = setup.customize(transfers, &[SLOT_UPPERCASE]);
+
+    // 3. ASSERT
+    tx_result.assert_ok();
+    assert_eq!(sc_result.unwrap(), 1u64);
+
+    // equippable & items sent burned
+    setup.assert_is_burn(EQUIPPABLE_TOKEN_ID, NONCE);
+
+    assert_eq!(
+        setup.blockchain_wrapper.get_esdt_balance(
+            &setup.first_user_address,
+            ITEM_TO_UNEQUIP_ID,
+            NONCE
+        ),
+        rust_biguint!(1),
+        "Item unequipped should be received"
+    );
+
+    assert_eq!(
+        setup.blockchain_wrapper.get_esdt_balance(
+            &setup.first_user_address,
+            EQUIPPABLE_TOKEN_ID,
+            1u64
+        ),
+        rust_biguint!(1),
+        "Equippable NFT should be received"
+    );
+
+    // is equippable empty
+    setup.blockchain_wrapper.check_nft_balance(
+        &setup.first_user_address,
+        EQUIPPABLE_TOKEN_ID,
+        1,
+        &rust_biguint!(1),
+        Option::Some(&EquippableNftAttributes::<DebugApi>::empty()),
+    );
+
+    setup.assert_uris(EQUIPPABLE_TOKEN_ID, 1, &[b"https://ipfs.io/ipfs/empty"]);
+}
+
+#[test]
 fn panic_when_unequip_twice_the_same_slot() {
     let mut setup = testing_utils::setup(customize_nft::contract_obj);
 

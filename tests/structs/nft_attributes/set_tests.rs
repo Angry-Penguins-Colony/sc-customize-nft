8d00f1cfd2e60dec5272822694176127be2ce81c
TF <31354550+Theo-Farnole@users.noreply.github.com>
2022-06-27 17:11:47 +0200

    FIX test that had #[should_panic]

--- tests/structs/nft_attributes/set_tests.rs
+++ tests/structs/nft_attributes/set_tests.rs
@@ -2,6 +2,8 @@ use customize_nft::structs::{equippable_nft_attributes::EquippableNftAttributes,
 use elrond_wasm::types::ManagedBuffer;
 use elrond_wasm_debug::DebugApi;
 
+use crate::testing_utils;
+
 #[test]
 fn set_item_on_empty_slot() {
     DebugApi::dummy();
@@ -23,24 +25,26 @@ fn set_item_on_empty_slot() {
 }
 
 #[test]
-#[should_panic]
 fn set_item_on_not_empty_slot() {
-    DebugApi::dummy();
+    let mut setup = testing_utils::setup(customize_nft::contract_obj);
 
-    let slot = &ManagedBuffer::new_from_bytes(b"hat");
+    setup
+        .blockchain_wrapper
+        .execute_query(&setup.cf_wrapper, |_sc| {
+            let slot = &ManagedBuffer::new_from_bytes(b"hat");
+            let mut equippable_nft_attributes = EquippableNftAttributes::<DebugApi>::new(&[(
+                slot,
+                Item {
+                    name: ManagedBuffer::new_from_bytes(b"item name"),
+                },
+            )]);
 
-    let mut equippable_nft_attributes = EquippableNftAttributes::<DebugApi>::new(&[(
-        slot,
-        Item {
-            name: ManagedBuffer::new_from_bytes(b"item name"),
-        },
-    )]);
-
-    // expect panic
-    equippable_nft_attributes.set_item(
-        &slot,
-        Option::Some(Item {
-            name: ManagedBuffer::new_from_bytes(b"item name"),
-        }),
-    );
+            equippable_nft_attributes.set_item(
+                &slot,
+                Option::Some(Item {
+                    name: ManagedBuffer::new_from_bytes(b"item name"),
+                }),
+            );
+        })
+        .assert_user_error("The slot is not empty. Please free it, before setting an item.");
 }

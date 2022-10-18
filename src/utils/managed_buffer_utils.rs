ed815711f6b9a14a7561e98957ac66b00321e953
TF <theo.farnole.pro@gmail.com>
2022-10-18 12:57:13 +0200

    FIXING C21: removed soem managed_buffer_utils functions

--- src/utils/managed_buffer_utils.rs
+++ src/utils/managed_buffer_utils.rs
@@ -11,19 +11,11 @@ pub trait ManagedBufferUtils<M: ManagedTypeApi> {
     fn load_512_bytes(&self) -> [u8; 512];
 
     fn split(&self, char: u8) -> ManagedVec<M, ManagedBuffer<M>>;
-    fn split_last_occurence(&self, char: u8) -> (ManagedBuffer<M>, ManagedBuffer<M>);
 
-    fn remove_first_char(&self) -> ManagedBuffer<M>;
-    fn remove_first_and_last_char(&self) -> ManagedBuffer<M>;
-
-    fn hex_to_u64(&self) -> Option<u64>;
-    fn ascii_to_u64(&self) -> Option<u64>;
     /// Set the first character to uppercase
     fn capitalize(&self) -> ManagedBuffer<M>;
-    fn equals_ignore_case(&self, other: &ManagedBuffer<M>) -> bool;
-    fn append_trailing_character_if_missing(&self, character: u8) -> ManagedBuffer<M>;
-    /// The replace method use new_buffer as ManagedBuffer because is it the easier way to implement
-    fn replace(&self, old_buffer: &[u8], new_buffer: &ManagedBuffer<M>) -> ManagedBuffer<M>;
+
+    /// The replace method use new_buffer as ManagedBuffer because is it the easier way to implement    
     fn contains(&self, to_find: &[u8]) -> bool;
     fn to_lowercase(&self) -> ManagedBuffer<M>;
     fn get_last_char(&self) -> u8;
@@ -71,71 +63,10 @@ impl<M: ManagedTypeApi> ManagedBufferUtils<M> for ManagedBuffer<M> {
         return output;
     }
 
-    fn split_last_occurence(&self, char: u8) -> (ManagedBuffer<M>, ManagedBuffer<M>) {
-        let bytes = self.load_512_bytes();
-
-        for i in (0..self.len() - 1).rev() {
-            if bytes[i] == char {
-                let first = ManagedBuffer::<M>::new_from_bytes(&bytes[..i]);
-                let second = ManagedBuffer::<M>::new_from_bytes(&bytes[i + 1..self.len()]);
-
-                return (first, second);
-            }
-        }
-
-        sc_panic_self!(M, "no occurence of char {:x} in bytes {:x}", char, self);
-    }
-
     fn get_last_char(&self) -> u8 {
         let bytes = self.load_512_bytes();
 
         return bytes[self.len() - 1];
-    }
-
-    fn remove_first_char(&self) -> ManagedBuffer<M> {
-        let bytes = self.load_512_bytes();
-
-        ManagedBuffer::new_from_bytes(&bytes[1..self.len()])
-    }
-
-    fn remove_first_and_last_char(&self) -> ManagedBuffer<M> {
-        let bytes = self.load_512_bytes();
-
-        return ManagedBuffer::new_from_bytes(&bytes[1..self.len() - 1]);
-    }
-
-    fn hex_to_u64(&self) -> Option<u64> {
-        let bytes = self.load_512_bytes();
-
-        let mut result: u64 = 0;
-
-        for i in bytes {
-            if i == 0 {
-                break;
-            }
-
-            result *= 16;
-            result += (i as char).to_digit(16)? as u64;
-        }
-
-        Some(result)
-    }
-
-    fn ascii_to_u64(&self) -> Option<u64> {
-        let bytes = self.load_512_bytes();
-
-        let mut result: u64 = 0;
-
-        for i in bytes {
-            if i == 0 {
-                break;
-            }
-
-            result *= 10;
-            result += (i as char).to_digit(16)? as u64;
-        }
-
-        Some(result)
     }
 
     fn capitalize(&self) -> ManagedBuffer<M> {
@@ -146,75 +77,6 @@ impl<M: ManagedTypeApi> ManagedBufferUtils<M> for ManagedBuffer<M> {
         // uppercase first letter
         o.append_bytes(&[bytes[0].to_ascii_uppercase()]);
         o.append_bytes(&bytes[1..self.len()]);
-
-        return o;
-    }
-
-    fn equals_ignore_case(self: &ManagedBuffer<M>, other: &ManagedBuffer<M>) -> bool {
-        if self.len() != other.len() {
-            return false;
-        }
-
-        let self_bytes = self.load_512_bytes();
-
-        let mut other_bytes: [u8; 512] = [0; 512];
-        other.load_to_byte_array(&mut other_bytes);
-
-        for i in 0..self.len() {
-            if self_bytes[i].to_ascii_lowercase() != other_bytes[i].to_ascii_lowercase() {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    fn append_trailing_character_if_missing(&self, character: u8) -> ManagedBuffer<M> {
-        let bytes = self.load_512_bytes();
-
-        let mut o = ManagedBuffer::<M>::new();
-
-        o.append_bytes(&bytes[0..self.len()]);
-
-        if bytes[self.len() - 1] != character {
-            o.append_bytes(&[character]);
-        }
-
-        return o;
-    }
-
-    fn replace(&self, old_buffer: &[u8], new_buffer: &ManagedBuffer<M>) -> ManagedBuffer<M> {
-        let bytes = self.load_512_bytes();
-
-        let mut o = ManagedBuffer::<M>::new();
-
-        let mut elements_to_skip = 0;
-
-        for i in 0..self.len() {
-            if elements_to_skip > 0 {
-                elements_to_skip -= 1;
-                continue;
-            }
-
-            if bytes[i] == old_buffer[0] {
-                for j in 0..old_buffer.len() {
-                    // is not a match, let's continue to the next character
-                    if bytes[i + j] != old_buffer[j] {
-                        o.append_bytes(&[bytes[i]]);
-                        break;
-                    }
-
-                    // is it a match
-                    if j == old_buffer.len() - 1 {
-                        o.append(new_buffer);
-                        elements_to_skip = j; // skip the old buffer
-                        break;
-                    }
-                }
-            } else {
-                o.append_bytes(&[bytes[i]]);
-            }
-        }
 
         return o;
     }
--- tests/utils/managed_buffer/mod.rs
+++ tests/utils/managed_buffer/mod.rs
@@ -1,14 +1,6 @@
-mod append_trailing_character_if_missing;
-mod ascii_to_u64;
 mod capitalize;
 mod compare;
 mod contains;
-mod equals_ignore_case;
-mod hex_to_u64;
 mod load_512_bytes;
-mod remove_first_and_last_char;
-mod remove_first_char;
-mod replace;
 mod split;
-mod split_last_occurence;
 mod to_lowercase;

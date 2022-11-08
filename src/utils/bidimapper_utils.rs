a9939fc1a003b599e2d8e018f078235dd67cf233
TF <theo.farnole.pro@gmail.com>
2022-11-08 18:20:08 +0100

    RETAKE contains_id and contains_value in BiDiMapper

--- src/utils/bidimapper_utils.rs
+++ src/utils/bidimapper_utils.rs
@@ -2,9 +2,12 @@ use elrond_wasm::{
     api::StorageMapperApi,
     elrond_codec::{NestedDecode, NestedEncode, TopDecode, TopEncode},
     storage::{mappers::BiDiMapper, StorageKey},
-    storage_get_len,
+    storage_get, storage_get_len,
     types::ManagedType,
 };
+
+const NULL_ENTRY: usize = 0;
+const ITEM_INDEX: &[u8] = b".index";
 
 pub trait ContainsUtils<SA, K, V> {
     fn contains_id(&self, id: &K, base_key: &[u8]) -> bool;
@@ -19,17 +22,19 @@ where
 {
     fn contains_id(&self, id: &K, base_key: &[u8]) -> bool {
         let mut key = StorageKey::<SA>::new(base_key);
-        key.append_bytes(b"_id_to_value");
+        key.append_bytes(b"_id");
+        key.append_bytes(ITEM_INDEX);
         key.append_item(id);
 
-        return storage_get_len(key.as_ref()) > 0;
+        storage_get::<SA, usize>(key.as_ref()) != NULL_ENTRY
     }
 
     fn contains_value(&self, value: &V, base_key: &[u8]) -> bool {
         let mut key = StorageKey::<SA>::new(base_key);
-        key.append_bytes(b"_value_to_id");
+        key.append_bytes(b"_value");
+        key.append_bytes(ITEM_INDEX);
         key.append_item(value);
 
-        return storage_get_len(key.as_ref()) > 0;
+        storage_get::<SA, usize>(key.as_ref()) != NULL_ENTRY
     }
 }

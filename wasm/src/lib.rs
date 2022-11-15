1255d89ceeaa0b1afe581fe7a84f1484491544bd
TF <theo.farnole.pro@gmail.com>
2022-11-15 15:33:39 +0100

    build wasm/src/lib.rs

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -15,6 +15,7 @@ elrond_wasm_node::wasm_endpoints! {
         getImagesToRender
         getItems
         getUriOf
+        overrideRoyalties
         registerItem
         renderImage
         setUriOfAttributes
